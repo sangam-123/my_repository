809fa947b01901461feacd1430a5ce7fb6a37500
Sangam Singh <sangsing126@gmail.com>
2020-09-06 09:18:36 +0530

    login functionality

---
+++ src/test/java/com/crm/qa/testcases/SampleGit.java
@@ -0,0 +1,34 @@
+package com.crm.qa.testcases;
+
+import org.testng.annotations.BeforeMethod;
+
+import com.crm.qa.base.TestBase;
+import com.crm.qa.pages.ContactsPage;
+import com.crm.qa.pages.HomePage;
+import com.crm.qa.pages.LoginPage;
+import com.crm.qa.util.TestUtil;
+
+public class SampleGit extends TestBase {
+	LoginPage loginPage;
+	HomePage homePage;
+	TestUtil testUtil;
+	ContactsPage contactsPage;
+
+	public SampleGit() {
+		super();
+	}
+
+	//test cases should be separated -- independent with each other
+	//before each test case -- launch the browser and login
+	//@test -- execute test case
+	//after each test case -- close the browser
+	
+	@BeforeMethod
+	public void setUp() {
+		initialization();
+		testUtil = new TestUtil();
+		contactsPage = new ContactsPage();
+		loginPage = new LoginPage();
+		homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+	}
+}
